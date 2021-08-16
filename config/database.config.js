b5837ab04512e59b8b0258f02cdb9a158b3dec63
himanshu034 <himanshu034@gmail.com>
2021-08-16 07:10:16 +0000

    Docker file and db connection

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-    url: 'mongodb://localhost:27017/easy-notes'
+    // url: 'mongodb://localhost:27017/easy-notes'
+    // url: 'mongodb+srv://admin:india123@cluster0.aytzo.mongodb.net/easy-notes?retryWrites=true&w=majority'
+    url: 'mongodb+srv://admin:india123@cluster0.t65zf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }

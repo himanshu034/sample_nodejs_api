3dbaf1d71abd22d9b3cbad3e7af9ed0e9ba85fdf
himanshu034 <himanshu034@gmail.com>
2026-02-04 18:45:38 +0530

    Delete sample_nodejs_demo_app directory

--- sample_nodejs_demo_app/anomalyDetection.js
+++
@@ -1,9 +0,0 @@
-const { detectAnomalies } = require('some-anomaly-detection-library'); // Replace with actual library
-
-function anomalyDetection(timeSeriesData) {
-    // Example implementation, replace with actual anomaly detection logic
-    const anomalies = detectAnomalies(timeSeriesData);
-    return anomalies;
-}
-
-module.exports = { anomalyDetection };
